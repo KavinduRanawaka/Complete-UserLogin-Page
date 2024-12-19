66c7a26da7a8ab52cb09a777df3f0da6f281386d
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-12-19 09:48:50 +0530

    enter password

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
     <div class="forms">
         <form action="" class="login">
             <h2>Login</h2>
+            <input type="text" name="email" placeholder="Entr your email...">
+            <input type="password" name="password" placeholder="Entr your password...">
             
         </form>
     </div>
