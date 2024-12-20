590b66695cf23a946f52c922d48bb7f5396fc002
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-12-20 13:55:40 +0530

    register

--- index.php
+++ index.php
@@ -10,15 +10,29 @@
 
    <!-- form -->
     <div class="forms">
+        <!-- login form -->
         <form action="" class="login">
             <h2>Login</h2>
             <input type="text" name="email" placeholder="Entr your email...">
             <input type="password" name="password" placeholder="Entr your password...">
             <div class="rem">
-                <input type="ckeckbox" name="re-check" id="re-check">
+                <input type="checkbox" name="re-check" id="re-check">
                 <label for="re-check">remember me</label>
             </div>
-            <button type="submit">Login</button>
+            <button type="submit" name="login-btn">Login</button>
+        </form>
+
+        <!-- Register form -->
+        <form action="" class="register">
+            <h2>Register</h2>
+            <input type="text" name="fname" placeholder="Entr your first Name...">
+            <input type="text" name="lname" placeholder="Entr your Last Name...">
+            <input type="text" name="email" placeholder="Entr your email...">
+            <input type="text" name="mobile" placeholder="Entr your mobile no...">
+            <input type="password" name="password" placeholder="Enter your password...">
+            <input type="password" name="re-pass" placeholder="Enter your password again...">
+            
+            <button type="submit" name="register-btn">Register</button>
         </form>
     </div>
 </body>
